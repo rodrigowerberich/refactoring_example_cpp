22f8370fdbe5d56ea53a53ecdc6336f984da33c0
Rodrigo Werberich <rodrigowerberich@hotmail.com>
2021-12-21 16:42:46 +0000

    Extract Function - AmountFor

--- components/refactoring/src/statement.cc
+++ components/refactoring/src/statement.cc
@@ -8,19 +8,53 @@
 namespace TheaterBilling
 {
 
-class comma_numpunct : public std::numpunct<char>
-{
-  protected:
-    virtual char do_thousands_sep() const
+    class comma_numpunct : public std::numpunct<char>
     {
-        return ',';
-    }
+    protected:
+        virtual char do_thousands_sep() const
+        {
+            return ',';
+        }
 
-    virtual std::string do_grouping() const
+        virtual std::string do_grouping() const
+        {
+            return "\03";
+        }
+    };
+
+    int amountFor(const Performance &perf, const Play &play)
     {
-        return "\03";
+        auto thisAmount = int(0);
+
+        switch (play.type())
+        {
+        case Play::Type::Tragedy:
+        {
+            thisAmount = 40000;
+            if (perf.audience() > 30)
+            {
+                thisAmount += 1000 * (perf.audience() - 30);
+            }
+            break;
+        }
+        case Play::Type::Comedy:
+        {
+            thisAmount = 30000;
+            if (perf.audience() > 20)
+            {
+                thisAmount += 10000 + 500 * (perf.audience() - 20);
+            }
+            thisAmount += 300 * perf.audience();
+            break;
+        }
+        case Play::Type::Invalid:
+            throw std::runtime_error("All performances need a valid play type.");
+        default:
+            throw std::runtime_error("Unsupported play type in invoice generation.");
+        }
+
+        return thisAmount;
     }
-};
 
     std::string statement(const Plays &plays, const Invoice &invoice)
     {
@@ -40,43 +74,17 @@ class comma_numpunct : public std::numpunct<char>
         for (const auto &perf : invoice.performances())
         {
             const auto play = plays.at(perf.playId());
-            auto thisAmount = int(0);
-
-            switch (play.type())
-            {
-            case Play::Type::Tragedy:
-            {
-                thisAmount = 40000;
-                if (perf.audience() > 30)
-                {
-                    thisAmount += 1000 * (perf.audience() - 30);
-                }
-                break;
-            }
-            case Play::Type::Comedy:
-            {
-                thisAmount = 30000;
-                if (perf.audience() > 20)
-                {
-                    thisAmount += 10000 + 500 * (perf.audience() - 20);
-                }
-                thisAmount += 300 * perf.audience();
-                break;
-            }
-            case Play::Type::Invalid:
-                throw std::runtime_error("All performances need a valid play type.");
-            default:
-                throw std::runtime_error("Unsupported play type in invoice generation.");
-            }
+            auto thisAmount = amountFor(perf, play);
 
             // add volume credits
-            volumeCredits+= std::max(perf.audience()-30, 0);
+            volumeCredits += std::max(perf.audience() - 30, 0);
             // add extra credit for every ten comedy attendees
-            if (Play::Type::Comedy == play.type()) volumeCredits +=  std::floor(static_cast<double>(perf.audience()) / 5.0);
+            if (Play::Type::Comedy == play.type())
+                volumeCredits += std::floor(static_cast<double>(perf.audience()) / 5.0);
 
             // print line for this order
             result << " " << play.name() << ": $" << format(static_cast<double>(thisAmount) / 100.0) << " (" << perf.audience() << " seats)" << std::endl;
-            totalAmount += thisAmount; 
+            totalAmount += thisAmount;
         }
 
         result << "Amount owed is $" << format(static_cast<double>(totalAmount) / 100.0) << std::endl;
